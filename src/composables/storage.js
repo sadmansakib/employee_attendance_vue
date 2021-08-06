995e0812f0fad371875b808f652ec4af5d266a3c
sadmansakib <ssadman8@gmail.com>
2021-08-06 15:34:47 +0600

    let's move tokens from localstorage to sessionstorage for better security

--- src/composables/storage.js
+++ src/composables/storage.js
@@ -3,21 +3,21 @@ const REFRESH_TOKEN = "refresh_token";
 
 export const storage = () => {
   const saveTokens = (payload) => {
-    window.localStorage.setItem(ACCESS_TOKEN, payload.access_token);
-    window.localStorage.setItem(REFRESH_TOKEN, payload.refresh_token);
+    window.sessionStorage.setItem(ACCESS_TOKEN, payload.access_token);
+    window.sessionStorage.setItem(REFRESH_TOKEN, payload.refresh_token);
   };
 
   const removeTokens = () => {
-    window.localStorage.removeItem(ACCESS_TOKEN);
-    window.localStorage.removeItem(REFRESH_TOKEN);
+    window.sessionStorage.removeItem(ACCESS_TOKEN);
+    window.sessionStorage.removeItem(REFRESH_TOKEN);
   };
 
   const getAccessToken = () => {
-    return window.localStorage.getItem(ACCESS_TOKEN);
+    return window.sessionStorage.getItem(ACCESS_TOKEN);
   };
 
   const getRefreshToken = () => {
-    return window.localStorage.getItem(REFRESH_TOKEN);
+    return window.sessionStorage.getItem(REFRESH_TOKEN);
   };
 
   return { saveTokens, removeTokens, getAccessToken, getRefreshToken };
